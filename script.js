e102c3073012b41e5c3ddb7367c8b80d8723092e
Catherine <catvo91@gmail.com>
2016-11-11 17:08:43 -0800

    second commit. etch and sketch achieved

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	var i = 1;
-	while (i <= 16)
+	while (i <= 500)
 	{
 		$('#smallWrapper').append("<div class = 'grid'> </div>");
 		i++;
 	}
+	$('.grid').mouseenter(function() {
+		$(this).addClass('etch');
+	})
 });
