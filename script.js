2d8e0fd3057f711a1cc686a168fa18878e57e0e7
Catherine <catvo91@gmail.com>
2016-11-13 16:05:22 -0800

    third commit

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 $(document).ready(function(){
 	var i = 1;
-	while (i <= 500)
-	{
+	var setting = 0;
+	while (i <= 500) {
 		$('#smallWrapper').append("<div class = 'grid'> </div>");
 		i++;
 	}
-	$('.grid').mouseenter(function() {
-		$(this).addClass('etch');
+	$('.buttons').click(function(){
+		$(this).toggleClass('highlight');
 	})
+	$('#norm').click(function(){
+		if (setting === 1) {
+			setting = 0;
+		} else {
+			setting = 1;
+		}
+		console.log(setting);
+			if (setting === 1) {
+				$('.grid').mouseenter(function() {
+					$(this).addClass('etch');
+				})		
+			}
+	})
+
+	$('#fade').click(function(){
+		if (setting === 2) {
+			setting = 0;
+		} else {
+			setting = 2;
+		}
+		console.log(setting);
+		if (setting === 2) {
+			$('.grid').mouseenter(function(){
+				$(this).fadeTo('slow', 0.5);
+			})
+			$('.grid').mouseleave(function(){
+				$(this).fadeTo('slow', 1);
+			})
+		}
+	})
+	$('#clear').click(function(){
+		$('.grid').removeClass('etch');
+	})
+
 });
+
