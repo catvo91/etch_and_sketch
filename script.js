2d38c2eecc2379b0a8b985a55878486697799856
Catherine <catvo91@gmail.com>
2016-11-15 14:51:45 -0800

    sixth commit??

--- script.js
+++ script.js
@@ -1,113 +1,67 @@
 $(document).ready(function(){
 	var i = 1;
-	var setting = 1;
+	var settingInt = 1;
 	var rainbow = ["#dc8c8c", "#918cdc", "#3bb980", "#e6db3b", "#e6ab3b", "#ea9be7"];
+	var options = ['#norm', '#emo', '#epilepsy', '#rainbow', '#fade'];
 	var hue = 1;
+	var askSize = 0;
 	var sketch = function() {
 	$('.grid').mouseenter(function() {
-		if (setting === 1 && hue === 1) {
+		if (settingInt === 1 && hue === 1) {
 			$(this).css('background-color', 'red');
 		}
-		else if (setting === 1 && hue === 2) {
+		else if (settingInt === 1 && hue === 2) {
 			$(this).css('background-color', '#ad6ead');
 		}
-		else if (setting === 1 && hue === 3) {
+		else if (settingInt === 1 && hue === 3) {
 			$(this).css('background-color', '#8ba1e2');
 		}
-		else if (setting === 2)
+		else if (settingInt === 2 && hue === 1)
 		{
-			$(this).fadeTo('slow', 0.5);
+			var currentOpacity = $(this).css('opacity');
+			$(this).css('background-color', 'red');
+			$(this).css('opacity', currentOpacity - 0.2);
 		}
-		else if (setting === 3)
+		else if (settingInt === 2 && hue === 2)
 		{
-			var random = rainbow[Math.floor(Math.random() * rainbow.length)];
-			$(this).css('background-color', random);
+			var currentOpacity = $(this).css('opacity');
+			$(this).css('background-color', '#ad6ead');
+			$(this).css('opacity', currentOpacity - 0.2);
 		}
-		else if (setting === 4)
+		else if (settingInt === 2 && hue === 3)
+		{
+			var currentOpacity = $(this).css('opacity');
+			$(this).css('background-color', '#8ba1e2');
+			$(this).css('opacity', currentOpacity - 0.2);
+		}
+
+		else if (settingInt === 3)
 		{
 			var random = rainbow[Math.floor(Math.random() * rainbow.length)];
 			$(this).addClass('etch');
 			$('.etch').css('background-color', random);
 		}
-		else if (setting === 5 && hue === 1)
+		else if (settingInt === 4)
 		{
-			var currentOpacity = $(this).css('opacity');
-			$(this).css('background-color', 'red');
-			$(this).css('opacity', currentOpacity - 0.2);
+			var random = rainbow[Math.floor(Math.random() * rainbow.length)];
+			$(this).css('background-color', random);
 		}
-		else if (setting === 5 && hue === 2)
+		else if (settingInt === 5)
 		{
-			var currentOpacity = $(this).css('opacity');
-			$(this).css('background-color', '#ad6ead');
-			$(this).css('opacity', currentOpacity - 0.2);
-		}
-		else if (setting === 5 && hue === 3)
-		{
-			var currentOpacity = $(this).css('opacity');
-			$(this).css('background-color', '#8ba1e2');
-			$(this).css('opacity', currentOpacity - 0.2);
+			$(this).fadeTo('slow', 0.5);
 		}
 	})
 	$('.grid').mouseleave(function(){
-		if (setting === 2) {
+		if (settingInt === 5) {
 			$(this).fadeTo('slow', 1);
 		}
 	})
-}
+	}
 	while (i <= 625) {
 		$('#smallWrapper').append("<div class = 'grid'> </div>");
 		i++;
 	}
 	sketch();
-	$('.buttons').click(function(){
-		if ($(this).hasClass('highlight')) {
-    		$(this).removeClass('highlight');
-    	} 
-    	else {
-    		$('.highlight').removeClass('highlight');
-	    	$(this).addClass('highlight');
-		}
-	})
-	$('#norm').click(function(){
-		if (setting === 1) {
-			setting = 0;
-		} else {
-			setting = 1;
-		}
-	})
-		$('#fade').click(function(){
-		if (setting === 2) {
-			setting = 0;
-		} else {
-			setting = 2;
-		}
-	})
-		$('#rainbow').click(function(){
-		if (setting === 3) {
-			setting = 0;
-		} else {
-			setting = 3;
-		}
-	})
-		$('#epilepsy').click(function(){
-		if (setting === 4) {
-			setting = 0;
-		} else {
-			setting = 4;
-		}
-	})
-		$('#emo').click(function(){
-		if (setting === 5) {
-			setting = 0;
-		} else {
-			setting = 5;
-		}
-	})
-	$('#clear').click(function(){
-		$('.grid').removeClass('etch');
-		$('.grid').css('opacity', 1);
-		$('.grid').css('background-color', 'pink');
-	})
 	$('#size1').click(function(){
 		$('#choice').replaceWith('<div id = "choice"> 5 x 5 </div>')
 		i = 1;
@@ -147,6 +101,19 @@ $(document).ready(function(){
 		}
 		sketch();
 	})
+	$('#customSize').click(function(){
+		i = 1;
+		askSize = prompt("How many squares per side? 1 - 100");
+		$('#choice').replaceWith('<div id = "choice"> Custom </div>')
+		$('.grid').remove(); 
+		while (i <= askSize * askSize) {
+			$('#smallWrapper').append("<div class = 'grid'> </div>");
+			$('.grid').css('height', (625 / askSize));
+			$('.grid').css('width', (625 / askSize));
+			i++;
+		}
+		sketch();
+	})
 	$('#red').click(function(){
 		hue = 1;
 		$('#choice2').replaceWith('<div id = "choice2"> Red </div>')
@@ -159,8 +126,35 @@ $(document).ready(function(){
 		hue = 3;
 		$('#choice2').replaceWith('<div id = "choice2"> Blue </div>')
 	})
+	$('.buttons').click(function(){
+		if ($(this).hasClass('highlight')) {
+    		$(this).removeClass('highlight');
+    	} 
+    	else {
+    		$('.highlight').removeClass('highlight');
+	    	$(this).addClass('highlight');
+		}
+	})
+	$('#clear').click(function(){
+		$('.grid').removeClass('etch');
+		$('.grid').css('opacity', 1);
+		$('.grid').css('background-color', 'pink');
+	})
+	var setting = function(n) {
+			$(options[n - 1]).click(function(){
+				if (settingInt === n) {
+					settingInt = 0;
+				}
+				else {
+					settingInt = n
+				}
+			})
+		}
+	setting(1);
+	setting(2);
+	setting(3);
+	setting(4);
+	setting(5);
 });
-
-
 
 
